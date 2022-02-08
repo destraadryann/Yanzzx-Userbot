9783fda79422b105495f06874334af8e3045e092
Yanzzx <96351009+destraadryann@users.noreply.github.com>
2022-02-08 10:15:29 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -320,11 +320,44 @@ for binary, path in binaries.items():
 
 # 'bot' variable
 if STRING_SESSION:
-    # pylint: disable=invalid-name
-    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+    session = StringSession(str(STRING_SESSION))
 else:
-    # pylint: disable=invalid-name
-    bot = TelegramClient("userbot", API_KEY, API_HASH)
+    session = "𝐘𝐚𝐧𝐳𝐳𝐱-𝐔𝐬𝐞𝐫𝐛𝐨𝐭"
+try:
+    bot = TelegramClient(
+        session=session,
+        api_id=API_KEY,
+        api_hash=API_HASH,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+
+
+async def checking():
+    gocheck = pybase64.b64decode("QHlhbnp6eHN1cHBvcnQ==")
+    checker = pybase64.b64decode("QFByb2plY3R5YW56eng==")
+    Input_gocheck = gocheck.decode('utf-8')
+    Input_checker = checker.decode('utf-8')
+    try:
+        await bot(GetSec(f"{Input_gocheck}"))
+    except BaseException:
+        pass
+    try:
+        await bot(GetSec(f"{Input_checker}"))
+    except BaseException:
+        pass
+
+with bot:
+    try:
+        bot.loop.run_until_complete(checking())
+    except BaseException:
+        LOGS.info(
+            "Join Support Group @yanzzxsupport and Channel @Projectyanzzx to see the updates of ubot"
+            "Don't Leave")
+        quit(1)
 
 
 async def check_botlog_chatid():
